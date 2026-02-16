b1ea3c38196d63e7ac783c99ba20dd047075eac2
Vaibhav Jain <jainvaibav671@gmail.com>
2026-02-16 19:49:31 +0530

    we did something

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,13 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { env } from "~/lib/env";
 
-const connectionString = env.master_db_url;
-const queryClient = postgres(connectionString, { ssl: "require" });
+function createDbConnection(connectionString: string) {
+  if (!connectionString) {
+    return null;
+  }
+  const client = postgres(connectionString, { ssl: "require" });
+  return drizzle(client);
+}
 
-export const master_db = drizzle(queryClient);
-
-const usersClient = postgres(env.users_db_url, { ssl: "require" });
-export const user_db = drizzle(usersClient);
-
-const contentClient = postgres(env.content_db_url, { ssl: "require" });
-export const content_db = drizzle(contentClient);
+export const master_db = createDbConnection(env.master_db_url);
+export const user_db = createDbConnection(env.users_db_url);
+export const content_db = createDbConnection(env.content_db_url);
