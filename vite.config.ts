021d0c7e79e48c94d9a16e881fb548b6f7117463
Vaibhav Jain <jainvaibav671@gmail.com>
2026-02-23 19:32:20 +0530

    vercel deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -38,6 +38,13 @@ export default defineConfig(({ mode }) => {
         },
       },
     },
+    ssr: {
+      noExternal: [
+        "@heroui/react",
+        "@heroui/styles",
+        "react-aria-components",
+      ],
+    },
     plugins: [
       excelToJsonPlugin(),
       tailwindcss(),
